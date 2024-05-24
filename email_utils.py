f3886a61b788c217ff8885260ec2b320b1ef17d5
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2024-05-24 18:23:09 -0300

    Create email_utils.py

---
+++ email_utils.py
@@ -0,0 +1,36 @@
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import logging
+
+def send_email(from_email, to_email, subject, body, attachment_filename):
+    smtp_server = 'mail.reals.com.br'
+    smtp_port = 465
+    smtp_username = os.getenv('SMTP_USERNAME')
+    smtp_password = os.getenv('SMTP_PASSWORD')
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        with open(attachment_filename, 'rb') as attachment:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f'attachment; filename= {attachment_filename}')
+        msg.attach(part)
+
+        server = smtplib.SMTP_SSL(smtp_server, smtp_port)
+        server.login(smtp_username, smtp_password)
+        server.sendmail(from_email, to_email, msg.as_string())
+        server.quit()
+        logging.info("E-mail enviado com sucesso!")
+    except Exception as e:
+        logging.error("Erro ao enviar e-mail: %s", e)
